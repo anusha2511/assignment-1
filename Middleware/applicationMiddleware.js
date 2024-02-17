8050a0f2c56e9834a1f394d6c8c6640ede32e39f
anusha2511 <anusha2511>
2024-02-17 11:49:48 +0530

    done

---
+++ Middleware/applicationMiddleware.js
@@ -0,0 +1,16 @@
+// middleware/applicationMiddleware.js
+
+function commonMiddleware(req, res, next) {
+    console.log('Common middleware executed');
+    next();
+}
+
+function myMiddleware(req, res, next) {
+    console.log('My middleware executed');
+    next();
+}
+
+module.exports = {
+    commonMiddleware,
+    myMiddleware
+};
